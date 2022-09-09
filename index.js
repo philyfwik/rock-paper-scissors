f09cdc0e46b91dc696eede1360eb00c8fd566a04
pokanlawon <pokanlawon@fieldsafesolutions.com>
2022-09-09 14:29:44 -0600

    take user choice and decide who win the round

--- index.js
+++ index.js
@@ -10,4 +10,14 @@ const getComputerChoice = () => {
         return 'scissors';
 }
 
-console.log(getComputerChoice());
+// function to play a single round of rock paper scissors
+const playRound = (playerSelection, computerSelection) => {
+    if ((playerSelection.toLowerCase()=='rock' && computerSelection=='scissors') || (playerSelection.toLowerCase()=='paper' && computerSelection=='rock') || (playerSelection.toLowerCase()=='scissors' && computerSelection=='paper'))
+        return `You Win! ${playerSelection.toLowerCase()} beats ${computerSelection}!`;
+    else if ((computerSelection=='rock' && playerSelection.toLowerCase()=='scissors') || (computerSelection=='paper' && playerSelection.toLowerCase()=='rock') || (computerSelection=='scissors' && playerSelection.toLowerCase()=='paper'))
+        return `You Lose! ${computerSelection} beats ${playerSelection.toLowerCase()}!`;
+    else
+        return `Its a tie! you both played ${playerSelection}!`;
+}
+
+console.log(playRound(prompt('rock, paper, scissors?'), getComputerChoice()));
