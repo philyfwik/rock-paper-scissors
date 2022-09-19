6286caef599880966cadb427d677620f277044b6
pokanlawon <pokanlawon@fieldsafesolutions.com>
2022-09-19 15:21:24 -0600

    implement button functionity for unlimited rounds

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const userButtons = document.querySelectorAll('.user');
+const result = document.getElementById('result');
+const userScore = document.getElementById('user-score');
+const compScore = document.getElementById('comp-score');
 
 // function returns either rock, paper or scissors randomly
 const getComputerChoice = () => {
@@ -15,40 +19,57 @@ const playRound = (playerSelection, computerSelection) => {
     if ((playerSelection.toLowerCase()=='rock' && computerSelection=='scissors') || 
         (playerSelection.toLowerCase()=='paper' && computerSelection=='rock') || 
         (playerSelection.toLowerCase()=='scissors' && computerSelection=='paper')) {
-        console.log(`You Win! ${playerSelection.toLowerCase()} beats ${computerSelection}!`);
+        result.textContent = `You Win! ${playerSelection.toLowerCase()} beats ${computerSelection}!`;
         return 'W';
     }
     else if ((computerSelection=='rock' && playerSelection.toLowerCase()=='scissors') || 
         (computerSelection=='paper' && playerSelection.toLowerCase()=='rock') || 
         (computerSelection=='scissors' && playerSelection.toLowerCase()=='paper')) {
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection.toLowerCase()}!`);
+            result.textContent = `You Lose! ${computerSelection} beats ${playerSelection.toLowerCase()}!`;
         return 'L';
     }
     else {
-        console.log(`Its a tie! you both played ${playerSelection}!`);
+        result.textContent = `Its a tie! you both played ${playerSelection}!`;
         return 'T';
     }
 }
 
 // function to play a 5 round game of rock paper scissors
-const playGame =() => {
-    let userScore = 0, compScore = 0;
+const playGame = () => {
+    let uScore = 0; 
+    let cScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        let result = playRound(prompt('rock, paper, scissors?'), getComputerChoice());
+    // for (let i = 0; i < 5; i++) {
+    //     let result = playRound(prompt('rock, paper, scissors?'), getComputerChoice());
 
-        if (result == 'W')
-            userScore++;
-        else if (result == 'L')
-            compScore++;
-    }
+    //     if (result == 'W')
+    //         userScore++;
+    //     else if (result == 'L')
+    //         compScore++;
+    // }
 
-    if (userScore > compScore)
-        return `You win the game with ${userScore}/5 wins!`;
-    else if (userScore < compScore)
-        return `Computer wins the game with ${compScore}/5 wins!`;
-    else
-        return 'It\'s a tie, try another round to see a winner!';
+    userButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            let roundResult = playRound(button.id, getComputerChoice());
+            if (roundResult == 'W') {
+                uScore++
+                userScore.textContent = `${uScore}`;
+            }
+            else if (roundResult == 'L') {
+                cScore++;
+                compScore.textContent = `${cScore}`;
+            }
+        })
+    });
+
+    // if (uScore > cScore)
+    //     return `You win the game with ${uScore}/5 wins!`;
+    // else if (uScore < cScore)
+    //     return `Computer wins the game with ${cScore}/5 wins!`;
+    // else
+    //     return 'It\'s a tie, try another round to see a winner!';
 }
 
-console.log(playGame());
+
+
+playGame();
